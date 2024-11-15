0c7f3486e1a4a89b12cbb3178e0a62add769836d
Joan Goyeau <jgoyeau@netflix.com>
2024-11-15 12:49:31 -0500

    Fix BSP with scala cli dep instead of the executable

--- project.scala
+++ project.scala
@@ -1,21 +1,35 @@
 // Main
-//> using scala 3.5.2
-//> using jvm 17
-//> using options -deprecation -feature -unchecked -Wvalue-discard -Wnonunit-statement -Wunused:implicits -Wunused:explicits -Wunused:imports -Wunused:locals -Wunused:params -Wunused:privates -new-syntax -Xfatal-warnings
+//> using scala "3.5.2"
+//> using jvm "17"
 
-//> using dependency ch.epfl.scala:bsp4j:2.1.1
-//> using dependency com.julianpeeters::avrohugger-core:2.8.4
-//> using dependency com.julianpeeters::avrohugger-filesorter:2.8.4
-//> using dependency com.lihaoyi::os-lib:0.11.3
-//> using dependency com.lihaoyi::sourcecode:0.4.2
-//> using dependency io.circe::circe-core:0.14.10
-//> using dependency io.circe::circe-generic:0.14.10
-//> using dependency io.circe::circe-parser:0.14.10
-//> using dependency org.scala-lang::scala3-compiler:3.5.2
-//> using dependency org.typelevel::alleycats-core:2.12.0
-//> using dependency org.typelevel::cats-core:2.12.0
-//> using dependency org.virtuslab.scala-cli:scala-cli-bsp:1.5.2
-//> using dependency org.virtuslab.scala-cli::cli:1.5.2
+//> using options "-Wnonunit-statement"
+//> using options "-Wunused:explicits"
+//> using options "-Wunused:implicits"
+//> using options "-Wunused:imports"
+//> using options "-Wunused:locals"
+//> using options "-Wunused:params"
+//> using options "-Wunused:privates"
+//> using options "-Wvalue-discard"
+//> using options "-Xfatal-warnings"
+//> using options "-deprecation"
+//> using options "-feature"
+//> using options "-new-syntax"
+//> using options "-unchecked"
+
+//> using dependency "ch.epfl.scala:bsp4j:2.1.1"
+//> using dependency "com.julianpeeters::avrohugger-core:2.8.4"
+//> using dependency "com.julianpeeters::avrohugger-filesorter:2.8.4"
+//> using dependency "com.lihaoyi::os-lib:0.11.3"
+//> using dependency "com.lihaoyi::sourcecode:0.4.2"
+//> using dependency "io.circe::circe-core:0.14.10"
+//> using dependency "io.circe::circe-generic:0.14.10"
+//> using dependency "io.circe::circe-parser:0.14.10"
+//> using dependency "org.scala-lang::scala3-compiler:3.5.2"
+//> using dependency "org.typelevel::alleycats-core:2.12.0"
+//> using dependency "org.typelevel::cats-core:2.12.0"
+//> using dependency "org.virtuslab.scala-cli::cli:1.5.3"
+//> using dependency "org.virtuslab.scala-cli:scala-cli-bsp:1.5.3"
 
 // Test
-//> using test.dependency org.scalameta::munit::1.0.2
+//> using test.dependency "org.scalameta::munit::1.0.2"
+
